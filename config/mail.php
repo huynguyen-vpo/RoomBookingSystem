e27325d5a888efcb5e3510a119769c2804504d20
Duyen Du <20110201@student.hcmute.edu.vn>
2024-04-22 15:34:04 +0700

    Add feature sending mail

--- config/mail.php
+++ config/mail.php
@@ -35,7 +35,7 @@ return [
     */
 
     'mailers' => [
-
+        
         'smtp' => [
             'transport' => 'smtp',
             'url' => env('MAIL_URL'),
@@ -46,6 +46,8 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
+            'auth_mode'  => null,
+            'verify_peer'       => false,
         ],
 
         'ses' => [
---
+++ resources/views/emails/booking-email.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <div>
+        <p>Dear {{ $name }}, </p>
+
+		<p> Thank you for choosing our hotel! Your reservation  is confirmed. We're thrilled to host you for a memorable stay.
+	    Please review your booking details below. If you have any questions or special requests, feel free to reach out.
+		We look forward to providing you with an exceptional experience. Safe travels! </p>
+
+		Warm regards,
+		Hotel </pre>
+		<img src="{{ $message->embed(public_path() . '/images/logo.png') }}" />
+    </div>
+</body>
+</html>
+    </div>
+</body>
+</html>
