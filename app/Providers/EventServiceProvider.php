a7119541c343aecc2700c71a772a89f4865c9475
Duyen Du <20110201@student.hcmute.edu.vn>
2024-04-25 09:49:48 +0700

    Add listener job

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Events\BookingProcessed;
+use App\Jobs\SendBookingConfirmationEmailJob;
+use App\Listeners\SendEmail;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -27,6 +30,9 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Event::listen(
+            BookingProcessed::class,
+            [SendEmail::class, 'handle']
+        );
     }
 }
